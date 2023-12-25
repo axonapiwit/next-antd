194194d05c2fe426aaca54b42b737556988d198f
axonapiwit <axon.apiwit@gmail.com>
2023-12-25 16:27:08 +0700

    kick start project

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+module.exports = {
+	reactStrictMode: true,
+}
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  // disable:process.env.NODE_ENV === 'development'
+})
 
-module.exports = nextConfig
+module.exports = withPWA()
